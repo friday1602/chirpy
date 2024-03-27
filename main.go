bb07b7a8d78b565db73056fa9f8f01526bbaa7a9
กฤษฎิ์ ศรีวงศ์พรธนา <krit.sr@NBTC.domain>
2024-03-27 16:50:29 +0700

    add middleware to count request

--- main.go
+++ main.go
@@ -1,9 +1,53 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
+
+func main() {
+	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+	fileServer := http.FileServer(http.Dir("./app"))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("/metrics", apiCfg.metrics)
+	mux.HandleFunc("/reset", apiCfg.reset)
+
+	fileServer = http.FileServer(http.Dir("./app/assets"))
+	mux.Handle("/app/assets/", http.StripPrefix("/app/assets", fileServer))
+
+	mux.HandleFunc("/healthz", readiness)
+
+	corsMux := middlewareCors(mux)
+
+	log.Print("starting server on :8080")
+	err := http.ListenAndServe(":8080", corsMux)
+	log.Fatal(err)
+}
+
+// middlewareMetrics gathers amout of request to the page
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+
+}
+
+// metrics prints counts to the body
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits)
+}
+
+// reset resets counts
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = 0
+}
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -19,25 +63,7 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func readiness(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Set("Content-Type", "")
 	w.WriteHeader(200)
 	w.Write([]byte("OK"))
-}
-
-func main() {
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./app"))
-	mux.Handle("/app/", http.StripPrefix("/app", fileServer))
-
-	fileServer = http.FileServer(http.Dir("./app/assets"))
-	mux.Handle("/app/assets/", http.StripPrefix("/app/assets", fileServer))
-
-	mux.HandleFunc("/healthz", readiness)
-
-	corsMux := middlewareCors(mux)
-
-	log.Print("starting server on :8080")
-	err := http.ListenAndServe(":8080", corsMux)
-	log.Fatal(err)
 }
